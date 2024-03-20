7dc1f1307a15cdec94f54399764d470e6849d910
Sadi Musa <sadimusa00@gmail.com>
2024-03-20 03:28:28 +0300

    done

---
+++ index.js
@@ -0,0 +1,8 @@
+// Arrow function expression for divide
+const divide = (a, b) => a / b;
+
+// Arrow function expression for square
+const square = x => x * x;
+
+// Arrow function expression for add
+const add = (a, b) => a + b;
